d4e4b3cbec8efdb42d19bde20e26291cf9efa4f5
Guangjun Li <guangjun.li@ebaotech.com>
2017-11-01 10:26:03 +0800

    Add configure file

---
+++ configure.py
@@ -0,0 +1,6 @@
+# 生成控制文件的配置说明
+sqlloader_configure = {"file_name_ext":"txt", "terminated_by":",", "enclosed_by":'"', "append_type":"replace", "nls_lang":"AMERICAN_AMERICA.ZHS16GBK"}
+
+# 生成创建中间表的控制选项, 真实的数据类型或者实际定义的数据类型
+create_table_configure = {"real_data_type": False}
+
